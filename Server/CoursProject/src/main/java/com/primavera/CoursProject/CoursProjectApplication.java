cd838b152c2a7f4598d1ceaa3b7ea5bb9cc052a7
judith-barberan <judithbarmar2@gmail.com>
2020-10-20 12:43:30 +0200

    added https://start.spring.io/ initial project

---
+++ Server/CoursProject/src/main/java/com/primavera/CoursProject/CoursProjectApplication.java
@@ -0,0 +1,13 @@
+package com.primavera.CoursProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursProjectApplication.class, args);
+	}
+
+}
---
+++ Server/CoursProject/src/test/java/com/primavera/CoursProject/CoursProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primavera.CoursProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

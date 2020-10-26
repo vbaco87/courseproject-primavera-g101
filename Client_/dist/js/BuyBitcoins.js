76487cb5e255c50dff477646d86d3860557c98c2
Víctor Barroso <vbarrosocorchero@gmail.com>
2020-10-26 08:56:05 +0100

    added payment form

--- Client_/dist/js/BuyBitcoins.js
+++ Client_/dist/js/BuyBitcoins.js
@@ -1,22 +1,23 @@
 var price = "";
-$(document).ready(function() {
-
-    setInterval(function(){
-        getBitcoinPrice();
-        updateAmountToPay();
-     }, 200);
-    
-   
+$(document).ready(function () {
+  setInterval(function () {
+    getBitcoinPrice();
+    updateAmountToPay();
+  }, 200);
 });
 
 function getBitcoinPrice() {
-    $.get("https://blockchain.info/ticker", (data) => {
-        price = data.EUR.sell;
-        $("#bitcoinPrice").text(price + data.EUR.symbol);
-    });
+  $.get("https://blockchain.info/ticker", (data) => {
+    price = data.EUR.sell;
+    $("#bitcoinPrice").text(price + data.EUR.symbol);
+  });
 }
 
-function updateAmountToPay(){
-    console.log($("#btp").val());
-        $("#amountToPay").attr("placeholder", price*$("#btp").val())
+function updateAmountToPay() {
+  $("#amountToPay").attr("placeholder", price * $("#btp").val());
+}
+
+function showPaymentForm() {
+  $("#paymentForm").show();
+  $("#buyBitcoins").hide();
 }

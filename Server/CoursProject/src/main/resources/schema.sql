7c14fd40b1c23534d73372dee003091c963fb60f
Víctor Barroso <vbarrosocorchero@gmail.com>
2020-11-09 10:56:43 +0100

    databse fix

--- Server/CoursProject/src/main/resources/schema.sql
+++ Server/CoursProject/src/main/resources/schema.sql
@@ -4,15 +4,15 @@ CREATE TABLE users
 (
   id VARCHAR (256) PRIMARY KEY ,
   name VARCHAR (20) ,
-  secondName VARCHAR (20),
+  second_name VARCHAR (20),
   email VARCHAR (255),
   password VARCHAR(70) NOT NULL DEFAULT '123456',
-  phoneNumber VARCHAR(15) ,
+  phone_number VARCHAR(15) ,
   birthday DATE,
   country VARCHAR (20),
   city VARCHAR (20),
-  cityhomeAddress VARCHAR (255),
-  userType NUMBER (2)
+  city_home_address VARCHAR (255),
+  user_type NUMBER (2)
 );
 
 DROP TABLE if EXISTS accounts;
@@ -26,10 +26,11 @@ CREATE TABLE accounts
 	FOREIGN KEY (user_id ) REFERENCES users(id)
 );
 
+-- id primary key
 DROP TABLE if EXISTS entries;
 CREATE TABLE entries
 ( 
- account_id VARCHAR (256) PRIMARY KEY,
+  account_id VARCHAR (256) PRIMARY KEY,
   id INT AUTO_INCREMENT,
   quantity DOUBLE,
   type VARCHAR2(7),
@@ -42,24 +43,24 @@ DROP TABLE if EXISTS auctions;
 
 CREATE TABLE auctions(
   id VARCHAR (256) PRIMARY KEY ,
-  id_creator VARCHAR (256) NOT NULL ,
+  creator_id VARCHAR (256) NOT NULL ,
   total_bitcoins NUMBER NOT NULL,
   price NUMBER NOT NULL,
   opening_date DATE NOT NULL,
-  close_date DATE NOT NULL,
-  FOREIGN KEY (id_creator) REFERENCES users(id)
+  closing_date DATE NOT NULL,
+  FOREIGN KEY (creator_id) REFERENCES users(id)
 );
 
 DROP TABLE if EXISTS bids;
 
 CREATE TABLE bids(
   id VARCHAR (256) PRIMARY KEY ,
-  id_user VARCHAR (256) NOT NULL,
-  id_auction VARCHAR (256) NOT NULL,
+  user_id VARCHAR (256) NOT NULL,
+  auction_id VARCHAR (256) NOT NULL,
   bitcoins NUMBER NOT NULL,
   amount NUMBER NOT NULL,
-  FOREIGN KEY (id_user) REFERENCES users(id),
-  FOREIGN KEY (id_auction) REFERENCES auctions(id)
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (auction_id) REFERENCES auctions(id)
 );
 
 DROP TABLE if EXISTS winners;

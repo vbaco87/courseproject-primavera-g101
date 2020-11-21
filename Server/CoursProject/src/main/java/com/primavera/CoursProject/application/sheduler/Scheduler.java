758f31a705de0a06c5d444e94fd4b10fbb564746
judith-barberan <judithbarmar2@gmail.com>
2020-11-21 18:13:39 +0100

    ...

---
+++ Server/CoursProject/src/main/java/com/primavera/CoursProject/application/sheduler/Scheduler.java
@@ -0,0 +1,52 @@
+package com.primavera.CoursProject.application.sheduler;
+
+import com.primavera.CoursProject.application.daos.AccountDAO;
+import com.primavera.CoursProject.application.daos.AuctionDAO;
+import com.primavera.CoursProject.application.daos.UserDAO;
+import com.primavera.CoursProject.application.dto.AuctionDTO;
+import com.primavera.CoursProject.application.dto.UserDTO;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class Scheduler {
+
+    AuctionDAO auctionDAO;
+    UserDAO userDAO;
+    AccountDAO accountDAO;
+
+    public Scheduler(AuctionDAO auctionDAO, UserDAO userDAO, AccountDAO accountDAO) {
+        this.auctionDAO = auctionDAO;
+        this.userDAO = userDAO;
+        this.accountDAO = accountDAO;
+    }
+
+    @Scheduled(cron="*/5 * * * * *")
+    public void doSomething() {
+        updateNewFinishedAuctions();
+    }
+
+    public void updateNewFinishedAuctions(){
+        List<AuctionDTO> auctions = auctionDAO.getNewFinishedAuctions();
+        if(!auctions.isEmpty()){
+            auctionDAO.setActiveState(auctions);
+            for(AuctionDTO a : auctions){
+                updateWinners(a);
+            }
+        }
+    }
+
+    public void updateWinners (AuctionDTO auction){
+        List<UserDTO> winners = userDAO.getWinners(auction.getId());
+
+        for (UserDTO winner : winners){
+            userDAO.saveWinner(winner, auction);
+            accountDAO.updateBitcoin(winner.getId(), QUANTITAT ¿? );
+        }
+
+        List<UserDTO> bidders = userDAO.getBidders(auction.getId()); // tots els participants
+        bidders.removeAll(winners); // nomès els que no han guanyat
+    }
+}
--- Server/CoursProject/src/main/resources/schema.sql
+++ Server/CoursProject/src/main/resources/schema.sql
@@ -49,6 +49,7 @@ CREATE TABLE auctions(
   price NUMBER NOT NULL,
   opening_date DATE NOT NULL,
   closing_date DATE NOT NULL,
+  active BOOLEAN NOT NULL,
   FOREIGN KEY (creator_id) REFERENCES users(id)
 );
 
@@ -70,7 +71,7 @@ CREATE TABLE winners(
   amount NUMBER,
   price NUMBER,
   auction_id VARCHAR(256),
-  user_id VARCHAR (256), 
+  user_id VARCHAR (256),
   FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (auction_id) REFERENCES auctions(id)
 );
@@ -90,7 +91,5 @@ CREATE TABLE transactions(
    transaction_date DATE,
    purchases_id VARCHAR (256),
    FOREIGN KEY (purchases_id) REFERENCES purchases(id)
-
-
 );
 

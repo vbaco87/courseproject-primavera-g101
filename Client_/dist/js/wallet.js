c9c5e1f06ceca0073f20206579b99c382af397f1
andreafero <afernandezreo@edu.tecnocampus.cat>
2020-11-30 12:20:22 +0100

    changes

--- Client_/dist/js/wallet.js
+++ Client_/dist/js/wallet.js
@@ -3,7 +3,7 @@ var euros;
 var bitcoins;
 var locked;
 var usable;
-
+var accountId = 963963963;
 var token = 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJqYmFyYmVyYW5AZWR1LnRlY25vY2FtcHVzLmNhdCIsImF1dGhvcml0aWVzIjpbeyJhdXRob3JpdHkiOiJST0xFX0FETUlOIn1dLCJpYXQiOjE2MDY3MzAzNjksImV4cCI6MTYwNzU1NDgwMH0.L7Kr5w9jM-uBifzSXGLWf4kwP-iwQjiRAfdOsUySnLZvfq76glwwpyZedyp-mNG5Xgk8VeT1Ijizs8QK91472w';
 
 

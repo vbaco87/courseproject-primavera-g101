d5c84be85d9fe2bac2548b59b9965c4dde4688a3
Víctor Barroso <vbarrosocorchero@gmail.com>
2021-04-13 22:47:47 +0200

    -

--- CoursProject/src/main/java/com/primavera/CoursProject/application/exceptions/AuctionDoesNotExistException.java
+++
@@ -1,7 +0,0 @@
-package com.primavera.CoursProject.application.exceptions;
-
-public class AuctionDoesNotExistException extends RuntimeException {
-    public AuctionDoesNotExistException (String auctionId){
-        super("Auction" + auctionId + " doesn't exist");
-    }
-}
--- CoursProject/src/main/java/com/primavera/CoursProject/application/exceptions/UnblockMoneyException.java
+++
@@ -1,7 +0,0 @@
-package com.primavera.CoursProject.application.exceptions;
-
-public class UnblockMoneyException extends RuntimeException {
-	  public UnblockMoneyException (String userId){
-	        super("User " + userId + " tried to unblock more money than he has.");
-	    }
-}
